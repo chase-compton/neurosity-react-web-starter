aa964980bcfb6a73d829e297d3d31fa96b7bc031
chase-compton <cscompton1@crimson.ua.edu>
2023-02-12 20:14:54 -0600

    sunday night

--- src/pages/Activities/Activities.js
+++ src/pages/Activities/Activities.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
 import './Activities.scss'
 
 const Activities = () => {
@@ -10,7 +12,23 @@ const Activities = () => {
                     Activities
                 </h1>
             </div>
-            
+            <div className='button-center'>
+                <button className='calm-button'>
+                    <span class="box">
+                        Calm <FontAwesomeIcon icon={faArrowRight} />
+                    </span>
+                </button>
+                <button className='focus-button'>
+                    <span class="box">
+                        Focus <FontAwesomeIcon icon={faArrowRight} />
+                    </span>
+                </button>
+                <button className='PBB-button'>
+                    <span class="box">
+                        Power By Band <FontAwesomeIcon icon={faArrowRight} />
+                    </span>
+                </button>
+            </div>
         </div>
     )
 };
